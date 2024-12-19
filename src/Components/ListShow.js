3e8c4e17453a0174ffd730a25adba570d792b61a
Pouyan-g <pouyangoshtasbi.h@gmail.com>
2024-12-19 11:39:58 +0330

    v0.44

--- src/Components/ListShow.js
+++ src/Components/ListShow.js
@@ -1,17 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import DataContex from "../Context/ContexManage";
 import axios from "axios";
 import { FcLike, FcDislike } from "react-icons/fc";
-import { MdMovieCreation } from "react-icons/md";
+import { MdMovieCreation, MdDeleteOutline } from "react-icons/md";
 import { FaBook, FaRegEdit } from "react-icons/fa";
 import { useState } from "react";
 
 function ListShow() {
   const [hoveredItemId, setHoveredItemId] = useState(null);
-  const HoverHandler = () => {
-    return <div></div>;
+  const { item, fetchitem, DeleteItem, edititem } = useContext(DataContex);
+  const [editBTN, SetEditBTN] = useState(false);
+  const [stateEditId, SetStateEditId] = useState();
+  const [newTitle, SetNewTitle] = useState();
+  const FormHandler = (event) => {
+    event.preventDefault();
+    SetNewTitle(event.target.value);
+    console.log(stateEditId);
+    edititem(stateEditId, newTitle);
+    SetEditBTN(false);
   };
-  const { item } = useContext(DataContex);
+  const ChangeHandler = (event) => {
+    SetNewTitle(event.target.value);
+  };
   const rendereditem = item.map((item) => {
     return (
       <div
@@ -21,10 +31,33 @@ function ListShow() {
         onMouseLeave={() => setHoveredItemId(null)}
       >
         {hoveredItemId === item.id ? (
-          <FaRegEdit className="cursor-pointer mb-2" />
+          <div className="flex space-x-2">
+            <FaRegEdit
+              className="cursor-pointer mb-2"
+              onClick={() => handleEditClick(hoveredItemId, item.title)}
+            />
+            <MdDeleteOutline
+              className="cursor-pointer mb-2"
+              onClick={() => DeleteItem(stateEditId)}
+            />
+          </div>
         ) : null}
+
         {item.moviebook === "book" ? <FaBook /> : <MdMovieCreation />}
-        <h1>{item.title}</h1>
+
+        {editBTN && stateEditId === item.id ? (
+          <form onSubmit={FormHandler}>
+            <input
+              type="text"
+              className="border-2"
+              onChange={ChangeHandler}
+              value={newTitle}
+            />
+            <button className="border-2">save</button>
+          </form>
+        ) : (
+          <h1>{item.title}</h1>
+        )}
         <p>{item.situt}</p>
 
         {item.situt === "I Wrote it" || item.situt === "I Watched it" ? (
@@ -37,6 +70,21 @@ function ListShow() {
       </div>
     );
   });
+
+  const handleEditClick = (id, title) => {
+    if (editBTN === false) {
+      SetEditBTN(true);
+      SetStateEditId(id);
+      SetNewTitle(title);
+    } else {
+      SetEditBTN(false);
+      SetStateEditId(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchitem();
+  }, [item]);
 
   return (
     <div>
--- src/Context/ContexManage.js
+++ src/Context/ContexManage.js
@@ -5,24 +5,44 @@ const DataContex = createContext();
 const Provider = ({ children }) => {
   const [item, setItem] = useState([]);
   const [OpenModal, SOpenModal] = useState(false);
-  const [id, setId] = useState(0);
+  const [id, setId] = useState();
 
-  const CreatHandler = async () => {
-    const resp = await axios.post("");
+  const ItemCreator = async (title, sitution, Like, moviebook) => {
+    const resp = await axios.post("http://localhost:3001/item", {
+      id: id,
+      title: title,
+      situt: sitution,
+      like: Like,
+      moviebook: moviebook,
+    });
+    setItem([...item, resp.data]);
+    setId(Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000);
   };
 
-  const ItemCreator = (title, sitution, Like, moviebook) => {
-    setItem([
-      ...item,
-      {
-        id: id,
-        title: title,
-        situt: sitution,
-        like: Like,
-        moviebook: moviebook,
-      },
-    ]);
-    setId(id + 1);
+  const DeleteItem = async (id) => {
+    const rep = await axios.delete(`http://localhost:3001/item/${id}`);
+    const respDelete = item.filter((_item) => {
+      return _item.id !== id;
+    });
+    setItem(respDelete);
+  };
+
+  const fetchitem = async () => {
+    const resp = await axios.get("http://localhost:3001/item");
+    setItem(resp.data);
+  };
+
+  const edititem = async (id, title) => {
+    const resp = await axios.put(`http://localhost:3001/item/${id}`, {
+      title,
+    });
+    const update = item.map((_item) => {
+      if (_item.id === id) {
+        return { ...item, ...resp.data };
+      }
+      return;
+    });
+    setItem(resp.data);
   };
 
   const valurToShare = {
@@ -30,6 +50,9 @@ const Provider = ({ children }) => {
     SOpenModal,
     ItemCreator,
     item,
+    fetchitem,
+    DeleteItem,
+    edititem,
   };
   return (
     <DataContex.Provider value={valurToShare}>{children}</DataContex.Provider>
